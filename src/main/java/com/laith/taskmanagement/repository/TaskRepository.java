a838cb4cfc3ee35d1cd90909ca298b25b289b49a
laith <laithalnaimi40@gmail.com>
2026-03-18 22:38:29 +0300

    Final commit - Task Management API

--- src/main/java/com/laith/taskmanagement/repository/TaskRepository.java
+++ src/main/java/com/laith/taskmanagement/repository/TaskRepository.java
@@ -1,16 +1,12 @@
 package com.laith.taskmanagement.repository;
 
-import com.laith.taskmanagement.model.TaskPriority;
-import com.laith.taskmanagement.model.TaskStatus;
+import com.laith.taskmanagement.model.Task;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.laith.taskmanagement.model.Task;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-import java.util.List;
 
 public interface TaskRepository extends JpaRepository<Task, Long>, JpaSpecificationExecutor<Task> {
 
